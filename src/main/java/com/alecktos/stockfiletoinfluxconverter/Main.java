bb767fe02595cfaa9e9b5355e252ffdb488ceca9
Alexander Persson <persson33@gmail.com>
2018-10-20 19:00:30 +0200

    initial commit

---
+++ src/main/java/com/alecktos/stockfiletoinfluxconverter/Main.java
@@ -0,0 +1,31 @@
+package com.alecktos.stockfiletoinfluxconverter;
+
+import com.alecktos.misc.DateTime;
+import com.alecktos.misc.FileHandler;
+import com.alecktos.misc.InfluxdbDAO;
+import com.alecktos.misc.LineFileReader;
+
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		LineFileReader lineFileReader = new LineFileReader();
+		List<String> lines = lineFileReader.getLinesFromFile(
+				"/Users/Alexander/Projects/stockfiletoinfluxconverter/disney.txt"
+		);
+
+		final InfluxdbDAO influxdbDAO = new InfluxdbDAO();
+
+		lines.remove(0);
+		lines.stream().forEach(line -> {
+			StockFileLineExtractor stockFileLineExtractor = new StockFileLineExtractor(line);
+			//stockFileLineExtractor.
+			influxdbDAO.writeInflux(
+					stockFileLineExtractor.getPriceFromRow(),
+					DateTime.createFromTimeStamp(stockFileLineExtractor.getTimeStamp()),
+					"stocks-test"
+			);
+		});
+	}
+}
